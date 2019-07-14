f9205cfaea6cb207ad8efdef78e8eba40c2d3637
simpart <simpart>
2019-07-14 16:37:56 +0900

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # mofron-layout-relative
-relative layout for mofron
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+relative layout of mofron
+
+## Feature
+ - components are placed offset by 'relative' style value
+
+# Install
+```
+npm install mofron mofron-layout-relative
+```
+
+# Sample
+```html
+<require>
+    <tag module="mofron-comp-text">Text</tag>
+    <tag module="mofron-layout-relative">Relat</tag>
+</require>
+
+<div layout=Relat("top","0.2rem")>
+    <Text>text 1</Text>
+    <Text>text 2</Text>
+    <Text>text 3</Text>
+</div>
+```
+# Parameter
+
+|Simple<br>Param | Parameter Name | Type | Description |
+|:--------------:|:---------------|:-----|:------------|
+| ◯  | type | string | direction type ["top"/"right"/"bottom"/"left"] |
+| ◯  | offset | string (size) | offset value |
+| | value | string | same as 'offset' parameter |
+| ◯  | multiples | boolean | true: offset value is multiples values to the component index |
+| | | | false: offset value not multiples |
+
--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 /**
  * @file mofron-layout-relative/index.js
  * @brief relative layout of mofron
+ * @feature components are placed offset by 'relative' style value
  * @author simpart
  */
 const mf = require('mofron');
 
 mf.layout.Relative = class extends mf.Layout {
+    /**
+     * initialize layout
+     *
+     * @param (string/object) string: 'type' parameter
+     *                        object: layout options
+     * @param (string) 'value' parameter
+     * @param (boolean) 'multiples' parameter
+     * @pmap type,offset,multiples
+     * @type private
+     */
     constructor (po, p2, p3) {
         try {
             super();
             this.name('Relative');
-            this.prmMap(['type', 'value', 'multiple']);
+            this.prmMap(['type', 'offset', 'multiples']);
             this.prmOpt(po, p2, p3);
         } catch (e) {
             console.error(e.stack);
@@ -18,17 +29,23 @@ mf.layout.Relative = class extends mf.Layout {
         }
     }
     
+    /**
+     * layout contents
+     * 
+     * @param (number) component index
+     * @param (component) target component
+     * @type private 
+     */
     contents (idx, tgt) {
         try {
             let setmgn = {};
             setmgn['position'] = 'relative';
-            if (null === this.type()) {
-                throw new Error('could not find type');
-            }
-            if (true === this.multiple()) {
-                setmgn[this.type()] = this.value().value() * (idx+1) + this.value().type();
+            
+            let val = mf.func.getSize(this.value());
+            if (true === this.multiples()) {
+                setmgn[this.type()] = val.value() * (idx+1) + val.type();
             } else {
-                setmgn[this.type()] = this.value().toString();
+                setmgn[this.type()] = this.value();
             }
             tgt.adom().style(setmgn);
         } catch (e) {
@@ -37,28 +54,65 @@ mf.layout.Relative = class extends mf.Layout {
         }
     }
     
+    /**
+     * direction type
+     * 
+     * @param (string) direction type ["top"/"right"/"bottom"/"left"]
+     * @return (string) direction type
+     * @type parameter
+     */
     type (prm) {
-        try { return this.member('type', ['top', 'right', 'bottom', 'left'], prm); } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    value (prm) {
         try {
-            return this.member(
-                'value',
-                ['Size'],
-                (undefined !== prm) ? mf.func.getSize(prm) : prm
-            );
+            return this.member('type', ['top', 'right', 'bottom', 'left'], prm, "top");
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    multiple (prm) {
-        try { return this.member('multiple', 'boolean', prm, true); } catch (e) {
+    /**
+     * offset value
+     *
+     * @param (string (size)) offset value
+     * @return (string (size)) offset value
+     * @type parameter
+     */
+    offset (prm) {
+        try {
+            if (undefined !== prm) {
+                mf.func.getSize(prm);
+            }
+            return this.member("value", "string", prm, "0rem");
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * same as 'offset'
+     *
+     * @param (string) same as 'offset' parameter
+     * @return (string) same as 'offset' return
+     * @type parameter
+     */
+    value (prm) {
+        try { return this.offset(prm); } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * multiples value flag
+     *
+     * @param (boolean) true: offset value is multiples values to the component index
+     *                  false: offset value not multiples
+     * @return (boolean) multiple flag
+     * @type parameter
+     */
+    multiples (prm) {
+        try { return this.member('multiples', 'boolean', prm, true); } catch (e) {
             console.error(e.stack);
             throw e;
         }
